e3e2fa33aa26355c2129b2a7bafae344143af542
nbngc <nbngc7@gmail.com>
2021-03-10 21:42:49 +0545

    Added folder of Assignment 3

---
+++ Assignment_3/2.py
@@ -0,0 +1,17 @@
+def checkleapyear(year):
+    if year%4 == 0:
+        if year%100 == 0:
+            if year%400==0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+year =5000
+if checkleapyear(year):
+    print("{0} is a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(year))
